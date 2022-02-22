3116ca4381992e14a907f91e4603a89703f378e6
fazleyrabby <fazley111@gmail.com>
2022-02-23 01:57:10 +0600

    experience fix

--- pages/index.js
+++ pages/index.js
@@ -80,6 +80,7 @@ export default function Home() {
                   <div class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                   <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{item.from} - {item.to}</time>
                   <h3 class="text-2xl mb-2 font-bold">{item.name}</h3>
+                  <p class="mb-2 text-sm text-gray-500">{item.role}</p>
                   <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{item.description}</p>
                   {/* <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn more <svg class="ml-2 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a> */}
                 </li>
