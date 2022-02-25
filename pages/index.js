4808c35888ccd9b2fc967be0f462272cb6471d48
fazleyrabby <fazley111@gmail.com>
2022-02-25 17:05:58 +0600

    es lint errors

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
+/*eslint-disable jsx-a11y/alt-text*/
+/*eslint-disable @next/next/no-img-element*/
 import Head from 'next/head';
 import Image from 'next/image';
 import { Container } from '../components/Container';
