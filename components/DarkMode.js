70e78d534ecd95fee594f5f7f5ad3bf9da8e9e41
fazleyrabby <fazley111@gmail.com>
2022-02-26 18:12:49 +0600

    project description updated

--- components/DarkMode.js
+++ components/DarkMode.js
@@ -9,7 +9,7 @@ export const DarkMode = () => {
     useEffect(() => setMounted(true), [])
 
     if (!mounted) return <button className='inline-flex hover:bg-gray-100 text-black dark:hover:text-black rounded  ml-auto  outline-none group overflow-hidden'>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 m-2 dark:stroke-white stroke-transparent" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 m-2 dark:stroke-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" /></svg></button>
 
     return (<button className='inline-flex hover:bg-gray-100 text-black dark:hover:text-black rounded ml-auto outline-none group'
