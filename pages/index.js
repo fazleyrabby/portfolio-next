d02395ea71f951b6f7baeddd6ac582e1f2ed45fa
fazleyrabby <fazley111@gmail.com>
2023-09-02 18:42:05 +0600

    UI fix

--- pages/index.js
+++ pages/index.js
@@ -64,10 +64,10 @@ export default function Home({ posts, cv }) {
         {/* Projects  */}
         <div className="lg:mb-48 mb-24" id="projects">
           <h1 className="text-4xl font-bold m-6 text-center">Projects</h1>
-          <div className="hover:blur mx-auto space-y-6 lg:space-y-0 lg:gap-6 lg:grid lg:grid-cols-2">
+          <div className="hover-parent mx-auto space-y-6 lg:space-y-0 lg:gap-6 lg:grid lg:grid-cols-2">
             {projects.map((item) => (
-              <div key={item.title}>
-                <div className="hover:blur-none rounded-lg shadow-md hover:shadow-xl hover:scale-105 transition-transform bg-white max-w-md overflow-hidden mx-auto">
+              <div key={item.title} className="hover-child">
+                <div className="rounded-lg shadow-md   transition-transform bg-white max-w-md overflow-hidden mx-auto">
                   <a
                     href={item.live || item.github || "#!"}
                     target="_blank"
